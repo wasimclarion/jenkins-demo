b688a33c2b0bc83bf8695ab6de0211bcdf5a6382
wasim <wasim.sayyed@clariontechnologies.co.in>
2016-08-24 13:57:02 +0530

    adds validate password class and test

---
+++ tests/ValidatePasswordTest.php
@@ -0,0 +1,9 @@
+<?php 
+class ValidatePasswordTest extends PHPUnit_Framework_TestCase{
+
+	public function testValidLength(){
+		$valPass = new ValidatePassword();
+		$this->assertFalse($valPass->validLength('1234'));
+	}
+}
+?>
