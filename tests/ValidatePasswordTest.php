cdf2e10accce39531e6049daae2057be502e7d8e
wasim <wasim.sayyed@clariontechnologies.co.in>
2016-08-24 19:59:30 +0530

    vi editor changes

--- tests/ValidatePasswordTest.php
+++ tests/ValidatePasswordTest.php
@@ -1,7 +1,8 @@
 <?php 
-class ValidatePasswordTest extends PHPUnit_Framework_TestCase{
+class ValidatePasswordTest extends PHPUnit_Framework_TestCase {
 
 	public function testValidLength(){
+
 		$valPass = new ValidatePassword();
 		$this->assertFalse($valPass->validLength('1234'));
 	}
